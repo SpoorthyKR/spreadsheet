2317c6c378bc4c151a73cb0d753d95092ce4bba3
Spoorthy Rajashekar <57374095+SpoorthyKR@users.noreply.github.com>
2021-04-09 15:08:46 -0400

    Add files via upload
    In this program I have created rows and columns of table where user can add details using react application.

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Table from './components/Table'
+
+const App = () =>
+  (<div style={{ width: 'max-content' }}>
+    <Table x={4} y={4} />
+  </div>)
+
+export default App
